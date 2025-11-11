845418c42ddaed67740b404bad455cff1d673007
laulaobm <laura@diehofers.at>
2025-11-11 21:58:38 +0100

    add: shelly component

---
+++ voltcast-frontend/src/app/shelly/shelly.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shelly',
+  imports: [],
+  templateUrl: './shelly.html',
+  styleUrl: './shelly.css',
+})
+export class Shelly {
+
+}

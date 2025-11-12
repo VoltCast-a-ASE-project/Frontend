f75792a85892cc138967afc0fb20e513892e91db
laulaobm <laura@diehofers.at>
2025-11-12 13:18:32 +0100

    update: app routes to redirect to dashboard for now

--- voltcast-frontend/src/app/app.routes.ts
+++ voltcast-frontend/src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes, RouterModule } from '@angular/router';
 import { Dashboard } from './components/dashboard/dashboard';
 import { Login } from './components/login/login';
-//import { ShellyPage } from './components/shelly-page/shelly-page';
 
 export const routes: Routes = [
-  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   {path: 'login', component: Login},
   {path: 'dashboard', component: Dashboard},
-  //{path: 'shelly', component: ShellyPage}
 ];

51b676f8c1302fa480412aa8bf19e9c8447e0dac
laulaobm <laura@diehofers.at>
2025-11-12 11:00:31 +0100

    add: dashboard tests

--- voltcast-frontend/src/app/components/dashboard/dashboard.spec.ts
+++ voltcast-frontend/src/app/components/dashboard/dashboard.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ApiService } from '../services/api';
+import { ApiService } from '../../services/api';
 import { provideHttpClient } from '@angular/common/http';
 import { Dashboard } from './dashboard';
 
